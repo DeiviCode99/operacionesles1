3c75e365533447b876a06dc7ece006d71b9f6f07
Steven Rey <reymaldonado.stevenarbey@gmail.com>
2026-09-03 13:33:29 +0000

    agregando menu de opciones 1

--- administrador.sh
+++ administrador.sh
@@ -1,7 +1,7 @@
-3!/bin/bash
+
+#!/bin/bash
 #Bucle para el menu
 while true; do
-	clear
     echo "========================================="
     echo "       SMART LINUX SYSTEM MONITOR"
     echo "              Autor: Andres"
@@ -14,9 +14,42 @@ while true; do
     echo "6. Generar diagnóstico del sistema"
     echo "0. Salir"
     echo "========================================="
-    echo -n "Seleccione una opción: "
-
     read -p "Selecciona una opción [1-4]: " opcion
     echo ""
-
-
+    case $opcion in 
+	1)
+		echo -n "Usuario actual: "
+		whoami
+		echo -n "DIrectorio actual: "
+		pwd
+		echo -n "nombre del equipo: "
+		hostname
+		echo  "Sistema operativo"
+		lsb_release -ds
+		echo  "Arquitectura"
+		uname -m
+		echo "Fecha y hora"
+		date
+		read -p "Presione Enter para continuar..."
+		;;
+	2)
+		echo ""
+		;;
+	3)
+		;;
+	4)
+		;;
+	5)
+		;;
+	6)
+		;;
+	0)
+		echo "Saliendo..."
+		exit 0
+		;;
+	*)
+		echo "Opcion no valida"
+		read -p "Presione enter para continuar..."
+		;;
+   esac
+done
