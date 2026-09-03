580466ee4ba31ba04afff9796794b411b2cb6ed3
Steven Rey <reymaldonado.stevenarbey@gmail.com>
2026-09-03 11:40:11 +0000

    Agregando primera parte del menu

---
+++ administrador.sh
@@ -0,0 +1,22 @@
+3!/bin/bash
+#Bucle para el menu
+while true; do
+	clear
+    echo "========================================="
+    echo "       SMART LINUX SYSTEM MONITOR"
+    echo "              Autor: Andres"
+    echo "========================================="
+    echo "1. Información general del sistema"
+    echo "2. Estado de memoria y disco"
+    echo "3. Administración de procesos"
+    echo "4. Información y prueba de red"
+    echo "5. Buscar archivos"
+    echo "6. Generar diagnóstico del sistema"
+    echo "0. Salir"
+    echo "========================================="
+    echo -n "Seleccione una opción: "
+
+    read -p "Selecciona una opción [1-4]: " opcion
+    echo ""
+
+
